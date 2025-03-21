b09f68da86e55f94030a31d751926129d3809330
Toshiaki Suzuki <dmmymp@gmail.com>
2025-03-21 13:26:14 +0800

    Initial commit: Complain Easy MVP
    Add core API routes and pages for Complain Easy MVP

---
+++ src/app/consumer-rights/page.tsx
@@ -0,0 +1,8 @@
+export default function ConsumerRights() {
+    return (
+      <div>
+        <h1>Consumer Rights</h1>
+        <p>This page will contain consumer rights information.</p>
+      </div>
+    );
+  }
---
+++ src/app/terms-of-use/page.tsx
@@ -0,0 +1,8 @@
+export default function TermsOfUse() {
+    return (
+      <div>
+        <h1>Terms of Use</h1>
+        <p>This page will contain the terms of use.</p>
+      </div>
+    );
+  }
